d441118d12409179395a90ded69e1ca8b93a4d7d
Lareb Nawab <lareb.indore@gmail.com>
2025-03-30 11:33:10 +0530

    Support custom delimiters

--- app.rb
+++ app.rb
@@ -3,7 +3,19 @@ class StringCalculator
     return 0 if numbers.empty?
 
     delimiter = ","
+    if numbers.start_with?("//")
+      parts = numbers.split("\n", 2)
+
+      # 🚀 Ensure input format includes a newline after delimiter declaration
+      unless parts[1]
+        raise "Invalid format: missing newline after delimiter declaration"
+      end
+
+      delimiter = parts[0][2..]
+      numbers = parts[1]
+    end
     numbers = numbers.gsub("\n", delimiter)
-    numbers.split(",").map(&:to_i).sum
+    nums = numbers.split(delimiter).map(&:to_i)
+    nums.sum
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -26,5 +26,9 @@ RSpec.describe StringCalculator do
     it "handles new lines as delimiters" do
       expect(StringCalculator.add("1\n2,3")).to eq(6)
     end
+
+    it "supports custom single-character delimiters" do
+      expect(StringCalculator.add("//;\n1;2")).to eq(3)
+    end    
   end
 end
