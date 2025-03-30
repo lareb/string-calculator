fba02a0a7d94623bccdd908ae55cd006967ae684
Lareb Nawab <lareb.indore@gmail.com>
2025-03-30 11:21:11 +0530

    Initialize StringCalculator

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -3,5 +3,8 @@ require 'rspec'
 
 RSpec.describe StringCalculator do
   describe ".add" do
+    it "returns 0 for an empty string" do
+      expect(StringCalculator.add("")).to eq(0)
+    end
   end
 end
