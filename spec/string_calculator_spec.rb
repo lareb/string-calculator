ba7ed609ef5635ccf389b814b726cc7e61a941e0
Lareb Nawab <lareb.indore@gmail.com>
2025-03-30 11:24:24 +0530

    Handle multiple numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -13,6 +13,10 @@ RSpec.describe StringCalculator do
 
     it "returns the sum of two numbers" do
       expect(StringCalculator.add("1,5")).to eq(6)
+    end
+
+    it "returns the sum of multiple numbers" do
+      expect(StringCalculator.add("1,2,3,4,5")).to eq(15)
     end    
   end
 end
